7942a9178c71b179b345bba0250035fe3cd6e693
awesomesauce4646 <mary_grellmann@yahoo.com>
2026-07-23 04:10:56 -0500

    added img in options

--- main_menu.gd
+++ main_menu.gd
@@ -6,7 +6,7 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://song.tscn")
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://song.tscn")
+	get_tree().change_scene_to_file("res://options.tscn")
 
 
 
