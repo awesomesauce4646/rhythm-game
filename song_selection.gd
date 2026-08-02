40b35ea4a7ac9d0e8254ccfc34ed4236ff9e0db3
awesomesauce4646 <mary_grellmann@yahoo.com>
2026-08-02 11:42:01 -0500

    new song upbeat

--- song_selection.gd
+++ song_selection.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_play_water_pressed() -> void:
 	get_tree().change_scene_to_file("res://water_song.tscn")
+
+
+func _on_play_upbeat_pressed() -> void:
+	get_tree().change_scene_to_file("res://upbeat_song.tscn")
---
+++ upbeat_conductor.gd
@@ -0,0 +1,15 @@
+extends AudioStreamPlayer
+
+@export var bpm = 132.51
+
+var beat := 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#pitch_scale = .5
+	beat = get_playback_position() * bpm / 60
+	pass
+	
+func _ready() -> void:
+	await get_tree().create_timer(2.0).timeout
+	play()
